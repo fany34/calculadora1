a69b2c0871287816e7ceba1b945a2bffead372a5
calculadora <qftmcx100pre@gmail.com>
2016-06-17 17:59:40 +0200

    operacion complementada

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -23,4 +23,12 @@ public class Calculadora {
     float restar(float a,float b){
     return a-b;
     }
+    float multiplicacion(float a,float b){
+        return a*b;
+    
+    }
+    float division(float a,float b)
+    {
+    return a/b;
+    }
 }
